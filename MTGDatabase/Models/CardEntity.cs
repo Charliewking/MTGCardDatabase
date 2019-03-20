c50d43592d617b93d3c56d973be016e844f0606c
Charlie King <charki@microsoft.com>
2019-03-20 17:26:19 -0400

    First Commit Baby!

---
+++ MTGDatabase/Models/CardEntity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MTGDatabase.Models
+{
+    public class CardEntity : TableEntity
+    {
+        public CardEntity(string cardSet, string cardName)
+        {
+            this.PartitionKey = cardSet;
+            this.RowKey = cardName;
+        }
+
+        public CardEntity() { }
+        public string name { get; set; }
+        public string color1 { get; set; }
+        public string color2 { get; set; }
+        public string color3 { get; set; }
+        public string rarity { get; set; }
+        public string convertedCost { get; set; }
+        public int numberInCollection { get; set; }
+    }
+}
